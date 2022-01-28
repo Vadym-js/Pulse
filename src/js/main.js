1876071ea8dab7e2d342edcec0ace6e6f119fe8e
Vadym Vasylenko <vasilenkovadim50@gmail.com>
2022-01-28 13:25:36 +0200

    End project

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,35 @@ $(document).ready(function(){
             ]
         }
     );
+    //modal
+
+    $('[data-modal=consultation]').on('click',function ()
+    {
+        $('.overlay,#consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function (){
+        $('.overlay, #thanks,#consultation,#order').fadeOut('slow');
+        }
+    );
+    $('.button_mini').on('click', function (){
+        $('.overlay,#order').fadeIn('slow');
+    });
+    $('form').submit(function (e){
+        e.preventDefault();
+
+        if(!$(this).valid()){
+            return;
+        }
+        $.ajax({
+            type:"POST",
+            url:"mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function (){
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        })
+        return false;
+    })
 });
